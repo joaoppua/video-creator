6551a987009d6b7978de9efb43497eb4bba6d72e
joaoppua <joaoppua@gmail.com>
2024-10-19 22:32:30 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,22 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async headers() {
-    return [
-      {
-        source: '/(.*)',
-        headers: [
-          {
-            key: 'Cross-Origin-Opener-Policy',
-            value: 'same-origin',
-          },
-          {
-            key: 'Cross-Origin-Embedder-Policy',
-            value: 'require-corp',
-          },
-        ],
-      },
-    ];
+  reactStrictMode: true,
+  webpack: (config) => {
+    config.resolve.fallback = { fs: false, path: false };
+    return config;
   },
-};
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
